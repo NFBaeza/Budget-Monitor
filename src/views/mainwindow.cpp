38e151879c52146791584f67fac82627d2d8bc80
natalia.baeza <natalia.baeza@sansano.usm.cl>
2026-02-12 23:00:10 -0300

    feat: add detailed UI view when selecting months in the saving report

--- src/views/mainwindow.cpp
+++ src/views/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "./ui_mainwindow.h"
 #include "widgets/dashboardwidget.h"
 #include "widgets/savingwidget.h"
+#include "widgets/monthreportwidget.h"
 #include "dialogs/monthselectordialog.h"
 
 extern const QString user_id = "68e6ff6b-9189-48be-a84b-03a5448e0b8b";
@@ -11,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setFixedSize(500, 300);
+    setMinimumSize(500, 300);
     connect(ui->CurrentMonthButton, &QPushButton::clicked, this, &MainWindow::onCurrentMonthPressed);
     connect(ui->SavingViewButton, &QPushButton::clicked, this, &MainWindow::onSavingPressed);
     connect(ui->pastMonthsButton, &QPushButton::clicked, this, &MainWindow::onPastMonthsButtonPressed);
@@ -27,7 +28,7 @@ void MainWindow::onPastMonthsButtonPressed() {
     connect(dialog, &MonthSelectorDialog::month_clicked, this, [this](QDate date) {
         MonthView *monthView = new MonthView(date, this);
         connect(monthView, &MonthView::backbutton_was_pressed, this, &MainWindow::showMainView);
-        setFixedSize(1080, 650);
+        setMinimumSize(1080, 650);
         setCentralWidget(monthView);
     });
     connect(dialog, &MonthSelectorDialog::close_was_pressed, this, &MainWindow::showMainView);
@@ -37,14 +38,20 @@ void MainWindow::onPastMonthsButtonPressed() {
 void MainWindow::onSavingPressed() {
     SavingView *dashboard = new SavingView(this);
     connect(dashboard, &SavingView::backbutton_was_pressed, this, &MainWindow::showMainView);
-    setFixedSize(1080, 650);
+    connect(dashboard, &SavingView::monthSelected, this, [this](QDate date) {
+        MonthReport *monthReport = new MonthReport(date, this);
+        connect(monthReport, &MonthReport::backToSavingButtonWasPressed, this, &MainWindow::onSavingPressed);
+        resize(660, 750);
+        setCentralWidget(monthReport);
+    });
+    resize(1080, 650);
     setCentralWidget(dashboard);
 }
 
 void MainWindow::onCurrentMonthPressed() {
     MonthView *dashboard = new MonthView(QDate::currentDate(), this);
     connect(dashboard, &MonthView::backbutton_was_pressed, this, &MainWindow::showMainView);
-    setFixedSize(1080, 650);
+    resize(1080, 650);
     setCentralWidget(dashboard);
 }
 
@@ -58,7 +65,7 @@ void MainWindow::showMainView() {
     ui->setupUi(this);
 
     // Restore the original window size
-    setFixedSize(500, 230);
+    resize(500, 230);
 
     // Reconnect the button signals
     connect(ui->CurrentMonthButton, &QPushButton::clicked, this, &MainWindow::onCurrentMonthPressed);
---
+++ src/widgets/monthreportwidget.cpp
@@ -0,0 +1,132 @@
+#include "widgets/monthreportwidget.h"
+#include "./ui_monthreportwidget.h"
+
+
+extern QString user_id;
+
+MonthReport::MonthReport(QDate dateSelected, QWidget *parent)
+    : QWidget(parent),
+      ui(new Ui::MonthReport),
+      date(dateSelected)  {
+
+    ui->setupUi(this);
+
+    connect(ui->backToSavingButton, &QPushButton::clicked, this, [this](){
+        emit backToSavingButtonWasPressed();
+    });
+
+    
+    QString month_name = QLocale().monthName(date.month());
+    month_name[0] = month_name[0].toUpper();
+    ui->titleMonthLabel->setText(QString("%1 %2").arg(month_name).arg(date.year()));
+
+    getTotalIncomeExpenseAmount(date);
+    createPieChart();
+    getExpenseBreakdown(date);
+
+}
+
+void MonthReport::getTotalIncomeExpenseAmount(QDate date){
+    QString lastDate = QDate(date.year(), date.month(), date.daysInMonth()).toString("yyyy-MM-dd");
+    QSqlDatabase db = DatabaseManager::instance().getDatabase();
+    QSqlQuery query(db);
+    query.prepare("SELECT c.type, SUM(t.amount) "
+                  "FROM money_transactions t "
+                  "JOIN categories c ON t.category_id = c.id "
+                  "WHERE t.date >= :start AND t.date <= :end AND t.user_id = :user "
+                  "GROUP BY c.type");
+
+    query.bindValue(":start", date.toString("yyyy-MM-dd"));
+    query.bindValue(":end", lastDate);
+    query.bindValue(":user", user_id);
+
+    if (query.exec()) {
+        while (query.next()) {
+            QString type = query.value(0).toString().toLower();
+            int total = query.value(1).toInt();
+            if(type == "expense"){
+                totalExpenses = total;
+            } else{
+                totalIncomes = total;
+            }
+        }
+    } else {
+        qDebug() << "[setAmountByCategory] ERROR:" << query.lastError().text();
+    }
+
+    totalSavings = totalIncomes - totalExpenses;
+    
+    ui->totalExpensesAmount->setText(QString::number(totalIncomes));
+    ui->totalIncomesAmount->setText(QString::number(totalExpenses));
+    ui->totalSavingsAmount->setText(QString::number(totalSavings));
+}
+
+
+void MonthReport::createPieChart() {
+    auto *series = new QPieSeries();
+
+    series->append("Expenses", totalExpenses);
+    series->append("Incomes", totalIncomes);
+    if (totalSavings > 0) {
+        QPieSlice *slice = series->append("Savings", totalSavings);
+        slice->setBrush(Qt::lightGray);
+    }
+
+    auto *chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Money Distribution");
+    chart->legend()->show();
+    chart->legend()->setAlignment(Qt::AlignBottom);   
+    chart->setTheme(QChart::ChartThemeDark);
+
+    for (QPieSlice *slice : series->slices()) {
+        if (slice->value() <= 0) {
+            slice->setLabelVisible(false);
+        } else {
+            double porcentaje = 100 * slice->percentage();
+            slice->setLabel(QString("%1: %2%").arg(slice->label()).arg(porcentaje, 0, 'f', 1));
+        }
+    }
+
+    ui->graphicsView->setChart(chart);
+    chart->setMargins(QMargins(6,6, 6, 6));
+    ui->graphicsView->setRenderHint(QPainter::Antialiasing);
+}
+
+void MonthReport::getExpenseBreakdown(QDate date){
+    QString lastDate = QDate(date.year(), date.month(), date.daysInMonth()).toString("yyyy-MM-dd");
+    QSqlDatabase db = DatabaseManager::instance().getDatabase();
+    QSqlQuery query(db);
+    query.prepare("SELECT c.name, SUM(t.amount) "
+                  "FROM money_transactions t "
+                  "JOIN categories c ON t.category_id = c.id "
+                  "WHERE t.date >= :start AND t.date <= :end AND t.user_id = :user AND c.type = 'expense' "
+                  "GROUP BY c.name");
+
+    query.bindValue(":start", date.toString("yyyy-MM-dd"));
+    query.bindValue(":end", lastDate);
+    query.bindValue(":user", user_id);
+
+    int row = 0;
+    ui->tableBreakdownWidget->setColumnCount(3);
+    ui->tableBreakdownWidget->setHorizontalHeaderLabels({"Category","Amount","%"});
+    ui->tableBreakdownWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableBreakdownWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    if (query.exec()) {
+        while (query.next()) {
+            ui->tableBreakdownWidget->insertRow(row);
+            QString category = query.value(0).toString().toLower();
+            category[0] = category[0].toUpper();
+            int total = query.value(1).toInt();
+            float percentOfExpenses = (total*100)/totalExpenses;
+
+            ui->tableBreakdownWidget->setItem(row, 0, new QTableWidgetItem(category));
+            ui->tableBreakdownWidget->setItem(row, 1, new QTableWidgetItem(QString::number(total)));
+            ui->tableBreakdownWidget->setItem(row, 2, new QTableWidgetItem(QString::number(percentOfExpenses, 'f', 1) + "%"));  
+            row++;       
+        }
+    } else {
+        qDebug() << "[setAmountByCategory] ERROR:" << query.lastError().text();
+    }
+}
