333c087b3a82d14d9232e8d28265e2f42800598b
natalia.baeza <natalia.baeza@sansano.usm.cl>
2026-03-12 11:21:31 -0300

    chore: clean code in .cpp and .h, optimized view and totals calculation

--- include/widgets/savingwidget.h
+++ include/widgets/savingwidget.h
@@ -1,15 +1,9 @@
 #ifndef SAVINGWIDGET_H
 #define SAVINGWIDGET_H
 
-#include "database/databasemanager.h"
-#include "widgets/monthreportwidget.h"
 #include "reportservices/monthlyreportservice.h"
-#include <iostream>
 #include <QWidget>
-#include <QLabel>
 #include <QDate>
-#include <QPushButton>
-#include <QSqlError>
 #include <QDebug>
 #include <QLocale>
 #include <QBarSeries>
@@ -18,59 +12,40 @@
 #include <QValueAxis>
 #include <QtCharts/QChart>
 #include <QtCharts/QChartView>
-#include <QMap>
 #include <QVector>
-#include <QLayout>
-#include <QSqlRecord>
-#include <QSqlQuery>
 
-
-namespace Ui { 
-    class SavingView;
-
-    struct MONEY{
-        MonthlyReportService::Totals totals;
-        QMap<QString, MonthlyReportService::Totals> cards;
-        QString date;
-    }; 
-}
+namespace Ui { class SavingView; }
 
 class SavingView : public QWidget {
     Q_OBJECT
 
 public:
     explicit SavingView(QWidget *parent = nullptr);
-
-    QDate dateViewSelected;
-    QString month_name;
-    QString monthFilter;
-    int totalIncomes = 0;
-    int totalExpenses = 0;
-    int savings = 0;
     ~SavingView();
 
 signals:
     void backbutton_was_pressed();
-    void monthSelected(QDate date); 
+    void monthSelected(QDate date);
 
 private slots:
     void backButtonWasPressed();
 
 private:
+    struct MonthData {
+        QDate                       date;
+        MonthlyReportService::Totals totals;
+    };
+
     Ui::SavingView *ui;
 
-    QSqlTableModel *categoryModel{nullptr};
-    QSqlTableModel *incomeModel{nullptr};
-    QSqlTableModel *expenseModel{nullptr};
-    QSqlRelationalTableModel *transactionsModel{nullptr};
-    int numberOfMonthInAYear = 12;
+    static constexpr int MAX_MONTHS = 12;
+    int displayMonths = MAX_MONTHS;
+    QVector<MonthData> m_monthData;
 
-    QVector<Ui::MONEY> moneyByMonth;
-
-    void initView();
-    void getAmountByMonth(const QString firstDate);
-    void updateBarGraph();
+    void precomputeData();
+    void refreshView();
     void updateSummary();
+    void updateBarGraph();
 };
 
 #endif
