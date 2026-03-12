66673221705b2d3746fefa40d62f70c982929a4e
natalia.baeza <natalia.baeza@sansano.usm.cl>
2026-03-12 14:42:43 -0300

    chore: fix typo

--- src/widgets/dashboardwidget.cpp
+++ src/widgets/dashboardwidget.cpp
@@ -241,7 +241,7 @@ void MonthView::updatePieChart() {
 
 void MonthView::initView(){
     QString currentDateTime = QDateTime::currentDateTime().toString("dd-MM-yyyy HH:mm");
-    ui->DateNowLabel->setText(QString("Current time:\n %1").arg(currentDateTime));
+    ui->DateNowLabel->setText(QString("Current time: %1").arg(currentDateTime));
 
     auto [firstDate, lastDate] = monthDateRange();
     monthFilter = QString("money_transactions.date >= '%1' AND money_transactions.date <= '%2' AND money_transactions.user_id = '%3'")
